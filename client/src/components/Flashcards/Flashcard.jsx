4b174ee869f02f65ed9014f66f11e87daa8cd8bf
tommy <tommy.suwunrut@gmail.com>
2020-08-03 21:35:15 -0400

    paginated cards

--- client/src/components/Flashcards/Flashcard.jsx
+++ client/src/components/Flashcards/Flashcard.jsx
@@ -4,30 +4,48 @@ import Card from 'react-bootstrap/Card';
 
 import { ListGroup, ListGroupItem } from 'react-bootstrap';
 
-const Flashcard = ({ flashcard }) => {
-  const [flip, setFlip] = useState(false);
+const Flashcard = ({ flashcard, handleNextCard }) => {
+  const [isCorrect, setIsCorrect] = useState(false);
+  const [flipped, setFlipped] = useState(false);
+
+  const handleRevealAnswer = (answer) => {
+    if (!flipped) {
+      setFlipped(true);
+    }
+    const isAnswerCorrect = answer === flashcard.answer;
+    setIsCorrect(isAnswerCorrect);
+  };
 
   return (
-    <Card
-      bg="info"
-      text="dark"
-      className={`card ${flip ? 'flip' : ''}`}
-      onClick={() => setFlip(!flip)}
-    >
-      <Card.Body className="front">
-        <Card.Text>{flashcard.question}</Card.Text>
-        <ListGroup className="flashcard-answers">
-          {flashcard.options.map((answer) => {
-            return (
-              <ListGroupItem bg="info" className="flashcard-answer">
-                {answer}
-              </ListGroupItem>
-            );
-          })}
-        </ListGroup>
+    <Card bg="info" text="dark" className={`card ${flipped ? 'flip' : ''}`}>
+      <Card.Body className={flipped ? 'back' : 'front'}>
+        {flipped ? (
+          <div>
+            <h1>This is the answer {flashcard.answer}</h1>
+            <button onClick={() => handleNextCard(isCorrect)}>
+              Next Question!
+            </button>
+          </div>
+        ) : (
+          <>
+            <Card.Text>{flashcard.question}</Card.Text>
+            <ListGroup className="flashcard-answers">
+              {flashcard.options.map((answer) => {
+                return (
+                  <ListGroupItem
+                    bg="info"
+                    className="flashcard-answer"
+                    onClick={() => handleRevealAnswer(answer)}
+                  >
+                    {answer}
+                  </ListGroupItem>
+                );
+              })}
+            </ListGroup>
+          </>
+        )}
       </Card.Body>
       <Card.Body className="back">{flashcard.correct_answer}</Card.Body>
-      {flip ? flashcard.correct_answer : flashcard.question}
     </Card>
   );
 };
